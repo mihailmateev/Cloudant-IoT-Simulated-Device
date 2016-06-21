2d8e045807ace099e36301462c173346ea09156d
Mihail Mateev <mihail.mateev@strypes.eu>
2016-06-21 19:25:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CloudantCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read config data from a command line parameters
+            var user = args[0];
+            var password = args[1];
+            var database = args[2];
+
+            //base request builder for all requests containing user name and database name
+            var handler = new HttpClientHandler { Credentials = new NetworkCredential(user, password) };
+
+            //create an http client
+            using (var client = CreateHttpClient(handler, user, database))
+            {
+
+                #region manage simple json document
+
+                var creationResponse = Create(client, new { name = "john", age = 15 });
+                PrintResponse(creationResponse);
+
+                var id = GetString("id", creationResponse);
+                var readResponse = Read(client, id);
+                PrintResponse(readResponse);
+
+                var returnedObj = GetString(readResponse);
+                PrintDocument(returnedObj);
+                readResponse = Read(client, id);
+
+                var rev1 = GetString("_rev", readResponse);
+                var updateResponse = Update(client, id, new { name = "john", age = 36, _rev = rev1 });
+                PrintResponse(updateResponse);
+
+                var rev2 = GetString("rev", updateResponse); 
+                var deleteResponse = Delete(client, id, rev2);
+                PrintResponse(deleteResponse);
+
+                #endregion manage simple json document
+
+
+                #region manage data from sensors
+
+                //unfinite loop until esc key is pressed
+                Console.WriteLine("Press ESC to stop");
+                do
+                {
+                    while (!Console.KeyAvailable)
+                    {
+
+                        Sensor sensor = Sensor.Generate();
+                        
+                        //var creationSensorResponse = Create(sensor.modified.ToString() , client, sensor);
+                        var creationSensorResponse = Create(client, sensor);
+                        PrintResponse(creationSensorResponse);
+
+                        var _id = GetString("id", creationSensorResponse);
+                        var readSensorResponse = Read(client, _id);
+                        PrintResponse(readSensorResponse);
+
+                        var returnedSensorObj = GetString(readSensorResponse);
+                        PrintDocument(returnedSensorObj);
+
+                        readSensorResponse = Read(client, _id);
+                        rev1 = GetString("_rev", readSensorResponse);
+                        var updateSensorResponse = Update(client, _id, new { origtime = sensor.origtime, displacement = sensor.displacement, temp = sensor.temp, hmdt = sensor.hmdt, modified = DateTime.Now.ToJavaScriptMilliseconds(), record = sensor.record, recordtitle = sensor.recordtitle, tags = sensor.tags + " and Updated", _rev = rev1 });
+                        PrintResponse(updateSensorResponse);
+                    }
+                } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+                #endregion manage data from sensors
+            }
+        }
+
+        #region Create
+        private static HttpResponseMessage Create(HttpClient client, object doc)
+        {
+            var json = JsonConvert.SerializeObject(doc, Formatting.None);
+            return client.PostAsync("", new StringContent(json, Encoding.UTF8, "application/json")).Result;
+        }
+
+        private static HttpResponseMessage Create(string id, HttpClient client, object doc)
+        {
+            var json = JsonConvert.SerializeObject(doc, Formatting.None);
+            return client.PostAsync(id, new StringContent(json, Encoding.UTF8, "application/json")).Result;
+        }
+        #endregion Create
+
+        #region Read
+        private static HttpResponseMessage Read(HttpClient client, string id)
+        {
+            return client.GetAsync(id).Result;
+        }
+        #endregion Read
+
+        #region Update
+        private static HttpResponseMessage Update(HttpClient client, string id, object doc)
+        {
+            var json = JsonConvert.SerializeObject(doc, Formatting.None);
+            return client.PutAsync(id, new StringContent(json, Encoding.UTF8, "application/json")).Result;
+        }
+        #endregion Update
+
+        #region Delete
+        private static HttpResponseMessage Delete(HttpClient client, string id, string rev)
+        {
+            return client.DeleteAsync(id + "?rev=" + rev).Result;
+        }
+        #endregion Delete
+
+        #region CreateHttpClient
+        private static HttpClient CreateHttpClient(HttpClientHandler handler, string user, string database)
+        {
+            return new HttpClient(handler)
+            {
+                BaseAddress = new Uri(string.Format("https://{0}.cloudant.com/{1}/", user, database))
+            };
+        }
+        #endregion CreateHttpClient
+
+        #region PrintResponse
+        private static void PrintResponse(HttpResponseMessage response)
+        {
+            Console.WriteLine("Status code: {0}", response.StatusCode);
+            Console.WriteLine(Convert.ToString(response));
+        }
+        #endregion PrintResponse
+
+        #region PrintDocument
+        private static void PrintDocument(string doc)
+        {
+            Console.WriteLine("Print document:");
+            Console.WriteLine(doc);
+        }
+        #endregion PrintDocument
+
+        #region GetString(string , HttpResponseMessage )
+        private static string GetString(string propertyName, HttpResponseMessage creationResponse)
+        {
+            using (var streamReader = new StreamReader(creationResponse.Content.ReadAsStreamAsync().Result))
+            {
+                var responseContent = (JObject)JToken.ReadFrom(new JsonTextReader(streamReader));
+                return responseContent[propertyName].Value<string>();
+            }
+        }
+        #endregion GetString(string , HttpResponseMessage )
+
+        #region GetString(HttpResponseMessage)
+        private static string GetString(HttpResponseMessage creationResponse)
+        {
+            using (var streamReader = new StreamReader(creationResponse.Content.ReadAsStreamAsync().Result))
+            {
+                var responseContent = (JObject)JToken.ReadFrom(new JsonTextReader(streamReader));
+                return responseContent.ToString();
+            }
+        }
+        #endregion GetString(HttpResponseMessage)
+    }
+
+
+    public static class DateTimeJavaScript
+    {
+        private static readonly long DatetimeMinTimeTicks =
+           (new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).Ticks;
+
+        public static long ToJavaScriptMilliseconds(this DateTime dt)
+        {
+            return (long)((dt.ToUniversalTime().Ticks - DatetimeMinTimeTicks) / 10000);
+        }
+    }
+}
---
+++ Sensor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CloudantCRUD
+{
+    #region Sensor
+    /// <summary>
+    /// Sensor class 
+    /// this class is used to manage data from sensors
+    /// </summary>
+    public class Sensor
+    {
+        public string _id;
+        public string recordtitle;
+        public string record;
+        public string origtime;
+        public int displacement;
+        public int temp;
+        public int hmdt;
+        public long modified;
+        public string tags;
+
+        static Random R = new Random();
+        static string[] sensorNames = new[] { "sensorA", "sensorB", "sensorC", "sensorD", "sensorE" };
+        //DateTime.Now.Date.Ticks - DateTime.Parse("01/01/1970 00:00:00").Date.Ticks
+        public static Sensor Generate()
+        {
+            var sensorname = sensorNames[R.Next(sensorNames.Length)];
+            var id = DateTime.Now.ToJavaScriptMilliseconds().ToString();
+            return new Sensor { origtime = DateTime.UtcNow.ToString(), displacement = R.Next(30, 70), temp = R.Next(70, 150), hmdt = R.Next(30, 70),  modified = DateTime.Now.ToJavaScriptMilliseconds(), recordtitle = sensorname,  record = sensorname + " record", _id = id, tags = "Generated" };
+        }
+
+        public static Sensor Update(Sensor sensor, string revision)
+        {
+            return new Sensor { origtime = sensor.origtime, displacement = sensor.displacement, temp = sensor.temp, hmdt = sensor.hmdt, modified = DateTime.UtcNow.Ticks, record = sensor.record, recordtitle = sensor.recordtitle };
+        }
+    }
+
+    #endregion Sensor
+}
